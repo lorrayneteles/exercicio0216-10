e44cce5e4e8c47fbc0cc569332cca627cc37c281
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 21:32:03 -0300

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,88 @@
+#Exercicio 01: Verificar se um número é positivo, negativo ou zero
+
+# numero = int(input('Digite um número'))
+# print(f'O número digitado é:{numero} ')
+
+# if numero > 0:
+#   print(f'Esse número é positivo')
+
+# elif numero == 0:
+#     print(f'O número é zero')
+# else:
+#     print(f'O número é negativo')
+
+
+# #Exercicio 02: Maioridade
+
+# idade = int(input('Digite a idade :'))
+# print(f' A idade é: {idade}')
+# if idade >= 18:
+#   print(f'Maioridade')
+# else:
+#   print(f' Menoridade')
+
+
+  #Exercicio 03 : Número par ou impar 
+
+
+# num = int(input('Digite um numero:'))
+# if num % 2 == 0:
+#     print(f'O numero {num} é par')
+# else:
+#     print(f'O numero {num} é impar')
+
+
+#Exercicio 04: Número maior 
+
+# numero1 = int(input('Digite um numero:'))
+# numero2 = int(input('Digite outro numero:'))
+# if numero1 > numero2:
+#     print(f'O numero {numero1} é maior que {numero2}')
+# else:
+#     print(f'O numero {numero2} é maior que {numero1}')
+
+
+
+#Exercicio 05:peça o valor da compra se o valor for maior que 100 aplique um desconto de 10% caso contrario exiba valor sem desconto
+
+# compra = float(input('Qual o valor da compra?'))
+# if compra > 100:
+#     desconto = compra * 0.1
+#     total = compra - desconto
+# else:
+#     total = compra
+# print(f'O valor da compra com desconto é R${total:.2f}')
+
+
+
+#Exercicio 06:peça ao usuario um numero e verifique se ele é multiplo de 5
+
+# numero = int(input('Digite um numero:'))
+# if numero % 5 == 0:
+#     print(f'O numero {numero} é multiplo de 5')
+# else:
+#     print(f'O numero {numero} não é multiplo de 5')
+
+
+#Exercicio 07: peça 3 notas ao usuario e calcule a media. Se a media for maior ou igual a 7 informe aprovado senao reprovado
+ 
+# nota1 = float(input('Digite a primeira nota:'))
+# nota2 = float(input('Digite a segunda nota:'))
+# nota3 = float(input('Digite a terceira nota:'))
+# media = (nota1 + nota2 + nota3) / 3
+# if media >= 7:
+#     print(f'Aprovado com media {media:.2f}')
+# else:
+#     print(f'Reprovado com media {media:.2f}')
+
+#Exercicio 08: Digite a senha
+
+# senha = input('Digite sua senha ')
+
+# if senha == 'python123_EFG':
+#    print(f'Acesso Permitido')
+# else:
+#    print(f'Senha Incorreta')
+
+
+#Exercicio 09: 
